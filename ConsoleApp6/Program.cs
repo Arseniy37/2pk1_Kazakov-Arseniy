9f57bdc8fce44b6f769db6d40c522cefbdd99ca4
Arseniy37 <91648645+Arseniy37@users.noreply.github.com>
2021-10-01 13:13:06 +0500

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()), a = 0, b = 1; do { b += a; a = b - a; } while (b <= n); if (b - n < n - a) ;
+            if (b - n < n - a)
+                Console.WriteLine(b);
+            else
+                Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
