21f83f54ae0ef8fbf481ee3097648368477b22ad
Arseniy37 <91648645+Arseniy37@users.noreply.github.com>
2021-10-29 16:54:07 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {int count = 0;
+
+          int[,] a = new int[5, 10];
+
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+               
+                for (int j = 0; j < 10; j++)
+                {
+                    a[i, j] = random.Next(100);
+                    Console.Write( a[i, j]);
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (a[i, j] == x)
+                        Console.WriteLine(a[i, j]);
+                }
+            }
+
+        }
+
